edeb5e306eb65d94979ad94ef23b79508e83a1e0
Davidlequnchen <chen1189@e.ntu.edu.sg>
2019-12-26 22:05:19 +0800

    update

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "C:/Users/MI/Desktop/Github/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "C:/opt/ros/melodic/x64".split(';') if "C:/opt/ros/melodic/x64" != "" else []
+underlay_workspaces = "C:/Users/MI/Desktop/Github/devel;C:/opt/ros/melodic/x64".split(';') if "C:/Users/MI/Desktop/Github/devel;C:/opt/ros/melodic/x64" != "" else []
---
+++ build/industrial_core/industrial_msgs/cmake/industrial_msgs-genmsg-context.py
@@ -0,0 +1,11 @@
+# generated from genmsg/cmake/pkg-genmsg.context.in
+
+messages_str = "C:/Users/MI/Desktop/Github/src/industrial_core/industrial_msgs/msg/DebugLevel.msg;C:/Users/MI/Desktop/Github/src/industrial_core/industrial_msgs/msg/DeviceInfo.msg;C:/Users/MI/Desktop/Github/src/industrial_core/industrial_msgs/msg/RobotMode.msg;C:/Users/MI/Desktop/Github/src/industrial_core/industrial_msgs/msg/RobotStatus.msg;C:/Users/MI/Desktop/Github/src/industrial_core/industrial_msgs/msg/ServiceReturnCode.msg;C:/Users/MI/Desktop/Github/src/industrial_core/industrial_msgs/msg/TriState.msg"
+services_str = "C:/Users/MI/Desktop/Github/src/industrial_core/industrial_msgs/srv/CmdJointTrajectory.srv;C:/Users/MI/Desktop/Github/src/industrial_core/industrial_msgs/srv/GetRobotInfo.srv;C:/Users/MI/Desktop/Github/src/industrial_core/industrial_msgs/srv/SetDrivePower.srv;C:/Users/MI/Desktop/Github/src/industrial_core/industrial_msgs/srv/SetRemoteLoggerLevel.srv;C:/Users/MI/Desktop/Github/src/industrial_core/industrial_msgs/srv/StartMotion.srv;C:/Users/MI/Desktop/Github/src/industrial_core/industrial_msgs/srv/StopMotion.srv"
+pkg_name = "industrial_msgs"
+dependencies_str = "trajectory_msgs;std_msgs"
+langs = "gencpp;geneus;genlisp;gennodejs;genpy"
+dep_include_paths_str = "industrial_msgs;C:/Users/MI/Desktop/Github/src/industrial_core/industrial_msgs/msg;trajectory_msgs;C:/Opt/ros/melodic/x64/share/trajectory_msgs/cmake/../msg;std_msgs;C:/Opt/ros/melodic/x64/share/std_msgs/cmake/../msg;geometry_msgs;C:/Opt/ros/melodic/x64/share/geometry_msgs/cmake/../msg"
+PYTHON_EXECUTABLE = "C:/Opt/python27amd64/python.exe"
+package_has_static_sources = '' == 'TRUE'
+genmsg_check_deps_script = "C:/Opt/ros/melodic/x64/share/genmsg/cmake/../../../lib/genmsg/genmsg_check_deps.py"
