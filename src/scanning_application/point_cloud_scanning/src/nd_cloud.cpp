71a01b374841eb0f95eb2dca074d09bdcfa02be1
Chen Lequn <48017967+Davidlequnchen@users.noreply.github.com>
2023-09-06 12:49:51 +0700

    Update code for microepsilon robviz

--- src/scanning_application/point_cloud_scanning/src/nd_cloud.cpp
+++ src/scanning_application/point_cloud_scanning/src/nd_cloud.cpp
@@ -29,6 +29,7 @@ public:
   typedef pcl::PointXYZ Point;
   typedef pcl::PointCloud<Point> PointCloud;
 
+
   NdCloud() {
     // sub_cloud = nh.subscribe<sensor_msgs::PointCloud2>("/usb_cam/cloud", 5,  &NdCloud::cbPointCloud, this);
     sub_cloud = nh.subscribe<sensor_msgs::PointCloud2>("/microepsilon/laser_scan_filtered", 5,  &NdCloud::cbPointCloud, this);
@@ -54,12 +55,12 @@ public:
       ros::Time stamp = (*cloud_msg).header.stamp;
       // tf_listener->waitForTransform("/world", "/camera0", stamp, ros::Duration(1.0));
       // tf_listener->lookupTransform("/world", "/camera0", stamp, transform);
-      tf_listener->waitForTransform("/world", "/scanCONTROL_2900-50_scanner_laser_link", stamp, ros::Duration(1.0));
-      tf_listener->lookupTransform("/world", "/scanCONTROL_2900-50_scanner_laser_link", stamp, transform);
+      tf_listener->waitForTransform("world", "scanCONTROL_2900-50_scanner_laser_link", stamp, ros::Duration(1.0));
+      tf_listener->lookupTransform("world", "scanCONTROL_2900-50_scanner_laser_link", stamp, transform);
       pcl_ros::transformAsMatrix(transform, matrix1); 
       // matrix1 is the transformation(trans, rot) between world frame and camera frame
-      tf_listener->waitForTransform("/world", "/workobject", stamp, ros::Duration(1.0));
-      tf_listener->lookupTransform("/world", "/workobject", stamp, transform);
+      tf_listener->waitForTransform("world", "workobject", stamp, ros::Duration(1.0));
+      tf_listener->lookupTransform("world", "workobject", stamp, transform);
       pcl_ros::transformAsMatrix(transform, matrix2);
       matrix = matrix2.inverse() * matrix1;
       // the matrix calculated based on matrix1 and matrix2 is the transformation matrix from workobject to camera0
@@ -67,7 +68,7 @@ public:
       // Transform a sensor_msgs::PointCloud2 dataset using an Eigen 4x4 matrix.
       // ROS_INFO_STREAM("Transform:" << matrix);
 
-      cloud_out.header.frame_id = "/workobject";
+      cloud_out.header.frame_id = "workobject";
       pub_cloud.publish(cloud_out);
     } catch (tf::TransformException ex) {
       ROS_ERROR("%s",ex.what());
@@ -80,8 +81,31 @@ public:
 };
 
 
+bool areKukaTopicsAvailable() {
+  ros::master::V_TopicInfo master_topics;
+  ros::master::getTopics(master_topics);
+
+  // Replace with the actual Kuka state topics you are looking for
+  std::string kuka_topic1 = "/tf";
+  std::string kuka_topic2 = "/joint_states";
+
+  for (ros::master::V_TopicInfo::iterator it = master_topics.begin(); it != master_topics.end(); ++it) {
+    const ros::master::TopicInfo& info = *it;
+    if (info.name == kuka_topic1 || info.name == kuka_topic2) {
+      return true;
+    }
+  }
+  return false;
+}
+
 int main (int argc, char** argv) {
   ros::init(argc, argv, "cloud");
+
+  // Wait for Kuka state topics to become available
+  while (!areKukaTopicsAvailable()) {
+    ROS_INFO("Waiting for Kuka state topics to become available...");
+    ros::Duration(1.0).sleep();
+  }
 
   NdCloud nd_cloud;
 
